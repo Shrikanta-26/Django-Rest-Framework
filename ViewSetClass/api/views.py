2608f00c18b4adc779b7e9e9ec155b579a9606e2
shrikanta <shrikantadutta108@gmail.com>
2025-12-25 22:11:49 +0600

    VieSet Class

---
+++ ViewSetClass/api/views.py
@@ -0,0 +1,86 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import viewsets
+from .models import Student
+from .serializers import StudentSerializer
+
+class StudentViewSet(viewsets.ViewSet):
+    def list(self, request):
+        print("Basename:",self.basename)
+        print("Action:",self.action)
+        print("Detail:",self.detail)
+        print("Suffix:",self.suffix)
+        print("Name:",self.name)
+        print("Description:",self.description)
+        stu = Student.objects.all()
+        serializer = StudentSerializer(stu, many=True)
+        return Response(serializer.data)
+    
+    def retrieve(self, request, pk=None):
+        print("Basename:",self.basename)
+        print("Action:",self.action)
+        print("Detail:",self.detail)
+        print("Suffix:",self.suffix)
+        print("Name:",self.name)
+        print("Description:",self.description)
+        id=pk
+        if id is not None:
+            stu =Student.objects.get(id=id)
+            serializer = StudentSerializer(stu)
+            return Response(serializer.data)
+        
+    def create(self,request):
+        print("Basename:",self.basename)
+        print("Action:",self.action)
+        print("Detail:",self.detail)
+        print("Suffix:",self.suffix)
+        print("Name:",self.name)
+        print("Description:",self.description)
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg':'Data created'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def update(self,request,pk):
+        print("Basename:",self.basename)
+        print("Action:",self.action)
+        print("Detail:",self.detail)
+        print("Suffix:",self.suffix)
+        print("Name:",self.name)
+        print("Description:",self.description)
+        id=pk
+        stu = Student.objects.get(id=id)
+        serializer = StudentSerializer(stu,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg':'Complete Data updated'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def partial_update(self,request,pk):
+        print("Basename:",self.basename)
+        print("Action:",self.action)
+        print("Detail:",self.detail)
+        print("Suffix:",self.suffix)
+        print("Name:",self.name)
+        print("Description:",self.description)
+        id=pk
+        stu = Student.objects.get(id=id)
+        serializer = StudentSerializer(stu,data=request.data,partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg':'Partial Data updated'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    def destroy(self,request,pk):
+        print("Basename:",self.basename)
+        print("Action:",self.action)
+        print("Detail:",self.detail)
+        print("Suffix:",self.suffix)
+        print("Name:",self.name)
+        print("Description:",self.description)
+        id=pk
+        stu = Student.objects.get(id=id)
+        stu.delete()
+        return Response({'msg':'Data Deleted'})     

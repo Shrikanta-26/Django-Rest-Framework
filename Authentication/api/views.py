97d6cb693a8785c98e80f885b2ceb4d6600496ab
shrikanta <shrikantadutta108@gmail.com>
2025-12-27 01:05:18 +0600

    Basic authentication, global authentication with various permission classes

---
+++ Authentication/api/views.py
@@ -0,0 +1,13 @@
+from rest_framework import viewsets
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated,IsAdminUser,AllowAny
+
+class StudentModelViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    # permission_classes = [AllowAny]
+    # permission_classes = [IsAdminUser] #api is accessed by those users who stuf status = true.

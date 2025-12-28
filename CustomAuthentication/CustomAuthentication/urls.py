221769562fb48297a82191838dfb8a5fbf751b91
shrikanta <shrikantadutta108@gmail.com>
2025-12-28 20:25:01 +0600

    Custom Authentication

--- CustomAuthentication/CustomAuthentication/urls.py
+++ CustomAuthentication/CustomAuthentication/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path,include
 from api import views
 from rest_framework.routers import DefaultRouter
-from api.auth import CustomAuthToken
+# from api.auth import CustomAuthToken
 
 
 #creating router object
@@ -17,8 +17,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include(router.urls)),
     path('auth/',include('rest_framework.urls', namespace="rest_framework"))
-
-    # path('auth/',include('rest_framework.urls', namespace="rest_framework"))
-    path('gettoken/',CustomAuthToken.as_view())
-
-]
+    # path('gettoken/',CustomAuthToken.as_view())
+    ]

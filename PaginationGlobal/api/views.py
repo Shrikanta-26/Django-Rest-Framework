a9291c3052f17fc0a0605bd9f2b9b98c35511069
shrikanta <shrikantadutta108@gmail.com>
2025-12-30 20:09:39 +0600

    Global Pagination

---
+++ PaginationGlobal/api/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .serializers import StudentSerializers
+from .models import Student
+from rest_framework.generics import ListAPIView
+
+# Create your views here.
+
+class StudentList(ListAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializers

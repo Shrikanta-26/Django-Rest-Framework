a19c07f1842f92f99beef59c7cbd2b685d9da0fc
shrikanta <shrikantadutta108@gmail.com>
2025-12-31 00:28:14 +0600

    Cursor pagination

---
+++ CursorPagination/api/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .serializers import StudentSerializers
+from .models import Student
+from rest_framework.generics import ListAPIView
+from .myapp import MyCursorPagination
+# Create your views here.
+
+class StudentList(ListAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializers
+    pagination_class = MyCursorPagination
